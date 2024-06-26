b4768366a81a9ec8e2ef153381f8fa389ac439b8
Julien Minville <minville.julien1@gmail.com>
2024-06-26 09:51:59 -0400

    add style component packages

---
+++ my_portefolio/app/components/header/navBar.tsx
@@ -0,0 +1,76 @@
+// components/Header.tsx
+import styled from "styled-components";
+import Link from "next/link";
+
+const StyledHeader = styled.header`
+	width: 100%;
+	position: fixed;
+	top: 3vh;
+	left: center;
+	background-color: rgba(5, 10, 11, 0.4);
+	z-index: 2;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const HeaderContainer = styled.div`
+	display: flex;
+`;
+
+const NavigationList = styled.ul`
+	display: flex;
+	padding: 10px 0;
+	border-bottom: 0.1px solid var(--text-color, rgb(170, 184, 199));
+	align-items: center;
+	justify-content: space-between;
+	white-space: nowrap;
+`;
+
+const NavigationItem = styled.li`
+	padding: 0 20px;
+`;
+
+const NavigationLink = styled.a`
+	color: rgb(170, 184, 199);
+	text-decoration: none;
+	font-size: 1.5rem;
+`;
+
+const Header: React.FC = () => {
+	return (
+		<StyledHeader>
+			<HeaderContainer>
+				<NavigationList>
+					<NavigationItem>
+						<Link href="#header" passHref>
+							<NavigationLink>À propos</NavigationLink>
+						</Link>
+					</NavigationItem>
+					<NavigationItem>
+						<Link href="#bio" passHref>
+							<NavigationLink>Bio</NavigationLink>
+						</Link>
+					</NavigationItem>
+					<NavigationItem>
+						<Link href="#skills" passHref>
+							<NavigationLink>Aptitudes</NavigationLink>
+						</Link>
+					</NavigationItem>
+					<NavigationItem>
+						<Link href="#work" passHref>
+							<NavigationLink>Projets</NavigationLink>
+						</Link>
+					</NavigationItem>
+					<NavigationItem>
+						<Link href="#footer" passHref>
+							<NavigationLink>Me joindre</NavigationLink>
+						</Link>
+					</NavigationItem>
+				</NavigationList>
+			</HeaderContainer>
+		</StyledHeader>
+	);
+};
+
+export default Header;
